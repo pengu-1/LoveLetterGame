1b398ed4538f5e78a5266e6292fc8d41fc65d1a3
Jack <pengjack2@gmail.com>
2020-06-10 12:03:34 +1000

    fixing socket connection

--- src/components/Room.js
+++ src/components/Room.js
@@ -4,7 +4,7 @@ import io from 'socket.io-client'
 import NotStarted from './RoomComponents/NotStarted'
 import Started from './RoomComponents/Started'
 
-let endPoint = "http://localhost:5000"
+let endPoint = "http://penguu.herokuapp.com"
 let socket
 const useStyles = makeStyles((theme) => ({
     '@global': {
